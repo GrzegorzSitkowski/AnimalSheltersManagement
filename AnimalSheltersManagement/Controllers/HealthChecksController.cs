2f3a28f607e9a35bc6e8da6c99d7f55e06c8521d
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-02-21 18:23:10 +0100

    Created HealthChecksController

---
+++ AnimalSheltersManagement/Controllers/HealthChecksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnimalSheltersManagement.Controllers
+{
+    [Route("api/hc")]
+    [ApiController]
+    public class HealthChecksController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<string>> GetAsync()
+        {
+            return "Healthy";
+        }
+    }
+}
