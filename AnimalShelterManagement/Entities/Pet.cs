1005a92ed0731f042acab05b78b943e4131a7814
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-02-28 22:42:09 +0100

    Added entity of Pet

---
+++ AnimalShelterManagement/Entities/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Domain.Entities
+{
+    public class Pet
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set;}
+        public string? Photos { get; set; }
+        public int? Age { get; set; }
+        public string? Description { get; set; }
+        public string? Details { get; set; }
+        public Guid? ShelterId { get; set; }
+        public Shelter Shelter { get; set; }
+    }
+}
