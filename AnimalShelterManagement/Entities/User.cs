10a93fa5b0f55c3ec06641f1e2f4aaa8ea44aa20
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-02-28 22:45:54 +0100

    Corrected Role entity

--- AnimalShelterManagement/Entities/User.cs
+++ AnimalShelterManagement/Entities/User.cs
@@ -8,12 +8,13 @@ namespace AnimalShelterManagement.Domain.Entities
 {
     public class User
     {
-        public Guid Id { get; set; }
-        public Role RoleId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string City { get; set; }
+        public Guid Id { get; set; }       
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? City { get; set; }
+        public Guid? RoleId { get; set; }
+        public Role? Role { get; set; }
     }
 }
