5c7fec1ccc2a30683f4aa96ca29e2f3c8811b80e
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-02-28 22:30:50 +0100

    Added entity of Role

---
+++ AnimalShelterManagement/Entities/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Domain.Entities
+{
+    public class Role
+    {
+        public Guid Id { get; set; }
+        public User UserId { get; set; }
+    }
+}
