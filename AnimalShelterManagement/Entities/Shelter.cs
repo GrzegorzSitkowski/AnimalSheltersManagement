6513cca74462fb296cadb7b7451583ccf06c7e1d
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-01 18:46:08 +0100

    Modified class of Shelter

--- AnimalShelterManagement/Entities/Shelter.cs
+++ AnimalShelterManagement/Entities/Shelter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnimalShelterManagement.Domain.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,7 @@ namespace AnimalShelterManagement.Domain.Entities
         public Guid Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
-        public string? Street {  get; set; }
-        public string? City { get; set; }
-        public string? PostalCode { get; set; }
+        public Address ShelterAddress { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public ICollection<Pet>? Pets { get; set; }
