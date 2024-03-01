f70ddb4f383eccd767e6b80f0e8d578fa3918e04
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-01 19:00:29 +0100

    Added AuditableEntity to Role.cs

--- AnimalShelterManagement/Entities/Role.cs
+++ AnimalShelterManagement/Entities/Role.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnimalShelterManagement.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,8 @@ using System.Threading.Tasks;
 
 namespace AnimalShelterManagement.Domain.Entities
 {
-    public class Role
+    public class Role : AuditableEntity
     {
-        public Guid Id { get; set; }
         public ICollection<User>? Users { get; set; }
     }
 }
