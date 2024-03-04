a38e581ae0d05d21b58e59d3b873a287988a1831
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-04 15:05:17 +0100

    Added and implemented ShelterDbContextFactory.cs

---
+++ AnimalShelterManagement.Persistance/ShelterDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using AnimalShelterManagement.Persistance;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShelterManagement.Persistance
+{
+    public class ShelterDbContextFactory : DesignTimeDbContextFactoryBase<ShelterDbContext>
+    {
+        protected override ShelterDbContext CreateNewInstance(DbContextOptions<ShelterDbContext> options)
+        {
+            return new ShelterDbContext(options);
+        }
+    }
+}
