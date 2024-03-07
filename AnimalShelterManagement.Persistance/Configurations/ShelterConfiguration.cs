3ffff683e592f80ac2ab28465215a92b7e06782a
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-07 17:28:23 +0100

    Created ShelterConfiguration

---
+++ AnimalShelterManagement.Persistance/Configurations/ShelterConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using AnimalShelterManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Persistance.Configurations
+{
+    public class ShelterConfiguration : IEntityTypeConfiguration<Shelter>
+    {
+        public void Configure(EntityTypeBuilder<Shelter> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.PhoneNumber).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.ShelterAddress).HasMaxLength(200).IsRequired();
+
+        }
+    }
+}
