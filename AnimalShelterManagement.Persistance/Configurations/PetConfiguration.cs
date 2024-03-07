2118c8ef689135d3cf39f18c204f7ccb41a56b5c
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-07 17:41:38 +0100

    Added PetConfiguration

---
+++ AnimalShelterManagement.Persistance/Configurations/PetConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AnimalShelterManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Persistance.Configurations
+{
+    public class PetConfiguration : IEntityTypeConfiguration<Pet>
+    {
+        public void Configure(EntityTypeBuilder<Pet> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(p => p.Name).HasMaxLength(50).IsRequired();
+            builder.Property(p => p.Age).HasMaxLength(10).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(200).IsRequired();
+        }
+    }
+}
