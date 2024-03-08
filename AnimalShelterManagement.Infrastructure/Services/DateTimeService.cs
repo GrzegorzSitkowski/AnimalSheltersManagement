9069d5736e639f306146f45813721fed8f0cb60d
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-08 15:45:08 +0100

    Implemented DateTimeSerivce

--- AnimalShelterManagement.Infrastructure/Services/DateTimeService.cs
+++ AnimalShelterManagement.Infrastructure/Services/DateTimeService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnimalShelterManagement.Application.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace AnimalShelterManagement.Infrastructure.Services
 {
     public class DateTimeService : IDateTime
     {
+        public DateTime Now => DateTime.Now;
     }
 }
