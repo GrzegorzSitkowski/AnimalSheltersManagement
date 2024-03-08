a701fe2b10aadbe091c037f0a0503217e836d0fc
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-08 16:09:13 +0100

    Created and Implemented FileWrapper

---
+++ AnimalShelterManagement.Infrastructure/FileStore/FileWrapper.cs
@@ -0,0 +1,17 @@
+﻿using AnimalShelterManagement.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Infrastructure.FileStore
+{
+    public class FileWrapper : IFileWrapper
+    {
+        public void WriteAllBytes(string outputFile, byte[] content)
+        {
+            File.WriteAllBytes(outputFile, content);
+        }
+    }
+}
