d75bdeca07868970ce7dc50ae973a10ab4b64209
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-08 15:50:20 +0100

    Implemented DepedencyInjection

---
+++ AnimalShelterManagement.Infrastructure/DepedencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using AnimalShelterManagement.Application.Interfaces;
+using AnimalShelterManagement.Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Infrastructure
+{
+    public static  class DepedencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient<IDateTime, DateTimeService>();
+            return services;
+        }
+    }
+}
