375d47f0430cc7e9d6aff2c46a236a30016cb872
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-12 18:53:38 +0100

    Added IshelterDbContext to DI

--- AnimalShelterManagement.Persistance/DepedencyInjection.cs
+++ AnimalShelterManagement.Persistance/DepedencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AnimalShelterManagement.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -14,6 +15,8 @@ namespace AnimalShelterManagement.Persistance
         public static IServiceCollection AddPersistance(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<ShelterDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("ShelterDatabase")));
+
+            services.AddScoped<IShelterDbContext, ShelterDbContext>();
 
             return services;
         }
