134d6b7a1308d50e6550ff551cadfd60575ae639
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-13 22:14:44 +0100

    Implementation ShelterDetailVm.cs

--- AnimalShelterManagement.Application/Shelters/Queries/GetShelterDetail/ShelterDetailVm.cs
+++ AnimalShelterManagement.Application/Shelters/Queries/GetShelterDetail/ShelterDetailVm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnimalShelterManagement.Domain.ValueObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace AnimalShelterManagement.Application.Shelters.Queries.GetShelterDetail
 {
     public class ShelterDetailVm
     {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public Address? ShelterAddress { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
