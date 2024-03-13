d28d3fef0a87718c38546e7b8aa0b38a4a375458
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-13 22:09:48 +0100

    GetShelterDetailQueryHandler.cs

---
+++ AnimalShelterManagement.Application/Shelters/Queries/GetShelterDetail/GetShelterDetailQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Application.Shelters.Queries.GetShelterDetail
+{
+    public class GetShelterDetailQueryHandler
+    {
+    }
+}
