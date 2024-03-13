60c1ffe59d96a4aabc76ddac604b5cfef3a21d55
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-13 22:19:36 +0100

    Configuration GetShelterDetailQuery.cs

--- AnimalShelterManagement.Application/Shelters/Queries/GetShelterDetail/GetShelterDetailQuery.cs
+++ AnimalShelterManagement.Application/Shelters/Queries/GetShelterDetail/GetShelterDetailQuery.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace AnimalShelterManagement.Application.Shelters.Queries.GetShelterDetail
 {
-    public  class GetShelterDetailQuery
+    public  class GetShelterDetailQuery : IRequest<ShelterDetailVm>
     {
         public Guid ShelterId { get; set; }
     }
