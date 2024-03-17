7a41d28d937e1f7c55f0e29dc753817fc8fb74c6
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-17 12:03:44 +0100

    Added GetSheltersQueryHandler.cs

---
+++ AnimalShelterManagement.Application/Shelters/Queries/GetShelters/GetSheltersQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AnimalShelterManagement.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Application.Shelters.Queries.GetShelters
+{
+    public class GetSheltersQueryHandler : IRequestHandler<GetSheltersQuery, SheltersVm>
+    {
+        private readonly IShelterDbContext _context;
+
+        public GetSheltersQueryHandler(IShelterDbContext shelterDbContext)
+        {
+                _context = shelterDbContext;
+        }
+
+        public async Task<SheltersVm> Handle(GetSheltersQuery request, CancellationToken cancellationToken)
+        {
+            var shelters = await _context.Shelters.AsNoTracking().Where(p => p.StatusId == 1).ToListAsync();
+
+            return new SheltersVm();
+        }
+    }
+}
