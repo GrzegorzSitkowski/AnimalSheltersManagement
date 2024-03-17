0ef04f393b97cebc8062d4a753ddff0eac419c03
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-17 12:27:23 +0100

    Implementation GetPetDetailQueryHandler.cs

---
+++ AnimalShelterManagement.Application/Pets/Queries/GetShelterDetail/GetPetDetailQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AnimalShelterManagement.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Application.Pets.Queries.GetShelterDetail
+{
+    public class GetPetDetailQueryHandler : IRequestHandler<GetPetDetailQuery, PetDetailVm>
+    {
+        private readonly IShelterDbContext _context;
+
+        public GetPetDetailQueryHandler(IShelterDbContext shelterDbContext)
+        {
+            _context = shelterDbContext;
+        }
+        public async Task<PetDetailVm> Handle(GetPetDetailQuery request, CancellationToken cancellationToken)
+        {
+            var pet = await _context.Pets.Where(p => p.Id == request.PetId).FirstOrDefaultAsync(cancellationToken);
+
+            var petVm = new PetDetailVm()
+            {
+                Name = pet.Name,
+                Photos = pet.Photos,
+                Age = pet.Age,
+                Description = pet.Description,
+                Details = pet.Details
+            };
+
+            return petVm;
+        }
+    }
+}
