d3e52200511fc0fc656b4a4172595082a5c7e9de
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-17 12:09:03 +0100

    Created GetShelterDetailVm.cs

---
+++ AnimalShelterManagement.Application/Pets/Queries/GetShelterDetail/GetShelterDetailVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Application.Pets.Queries.GetShelterDetail
+{
+    public class GetShelterDetailVm
+    {
+        public string? Name { get; set; }
+        public string? Photos { get; set; }
+        public int? Age { get; set; }
+        public string? Description { get; set; }
+        public string? Details { get; set; }
+    }
+}
