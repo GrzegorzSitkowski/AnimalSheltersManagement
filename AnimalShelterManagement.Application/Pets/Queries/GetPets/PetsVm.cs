8fb91b675b837f71b27964433bfa9b69c0eed4bc
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-17 12:30:12 +0100

    Created PetsVm.cs

---
+++ AnimalShelterManagement.Application/Pets/Queries/GetPets/PetsVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Application.Pets.Queries.GetPets
+{
+    public class PetsVm
+    {
+        public ICollection<PetsDto> Pets { get; set; }
+    }
+}
