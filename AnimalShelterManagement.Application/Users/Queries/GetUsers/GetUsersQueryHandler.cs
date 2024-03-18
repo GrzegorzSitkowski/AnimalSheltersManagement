22b1a4fdf0bdcd97826a317eabbebb11ff490ad1
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-18 15:41:13 +0100

    Implamentation GetUsersQueryHandler.cs

---
+++ AnimalShelterManagement.Application/Users/Queries/GetUsers/GetUsersQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AnimalShelterManagement.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Application.Users.Queries.GetUsers
+{
+    public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, UsersVm>
+    {
+        private readonly IShelterDbContext _context;
+
+        public GetUsersQueryHandler(IShelterDbContext shelterDbContext)
+        {
+            _context = shelterDbContext;
+        }
+
+        public async Task<UsersVm> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _context.Users.AsNoTracking().Where(p => p.StatusId == 1).ToListAsync();
+
+            return new UsersVm();
+        }
+    }
+}
