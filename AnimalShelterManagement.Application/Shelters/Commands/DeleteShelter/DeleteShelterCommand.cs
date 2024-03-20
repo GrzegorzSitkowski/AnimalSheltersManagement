69bad3256a715bd03c16caf2285f4bdfa565d83a
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-20 16:58:56 +0100

    Added DeleteShelterCommand

---
+++ AnimalShelterManagement.Application/Shelters/Commands/DeleteShelter/DeleteShelterCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Application.Shelters.Commands.DeleteShelter
+{
+    public class DeleteShelterCommand : IRequest
+    {
+        public Guid ShelterId { get; set; }
+    }
+}
