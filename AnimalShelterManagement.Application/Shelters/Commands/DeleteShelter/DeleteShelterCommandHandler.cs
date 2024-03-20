7ad9543c5a61514ce29281e600f7144fb1eb1759
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-20 17:06:11 +0100

    Implamantation DeleteShelterCommandHandler.cs

---
+++ AnimalShelterManagement.Application/Shelters/Commands/DeleteShelter/DeleteShelterCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AnimalShelterManagement.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Application.Shelters.Commands.DeleteShelter
+{
+    public class DeleteShelterCommandHandler : IRequestHandler<DeleteShelterCommand>
+    {
+        private readonly IShelterDbContext _context;
+
+        public DeleteShelterCommandHandler(IShelterDbContext shelterDbContext)
+        {
+            _context = shelterDbContext;
+        }
+
+        public async Task<Unit> Handle(DeleteShelterCommand request, CancellationToken cancellationToken)
+        {
+            var shelter = await _context.Shelters.Where(p => p.Id == request.ShelterId).FirstOrDefaultAsync(cancellationToken);
+
+            _context.Shelters.Remove(shelter);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
