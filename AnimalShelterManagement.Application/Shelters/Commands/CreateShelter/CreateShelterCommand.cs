ce10f58422c5977a8a3ef918e1f4d99bba843d06
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-20 16:45:13 +0100

    Added CreateShelterCommand.cs

---
+++ AnimalShelterManagement.Application/Shelters/Commands/CreateShelter/CreateShelterCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelterManagement.Application.Shelters.Commands.CreateShelter
+{
+    public class CreateShelterCommand : IRequest<int>
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string ShelterAddress { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
