0f6df1e933c604eeca2e2ce40f66985692335be3
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-25 20:34:14 +0100

    Mapping Vm in GetSheltersQueryHandler

--- AnimalShelterManagement.Application/Shelters/Queries/GetShelters/GetSheltersQueryHandler.cs
+++ AnimalShelterManagement.Application/Shelters/Queries/GetShelters/GetSheltersQueryHandler.cs
@@ -1,4 +1,6 @@
 ﻿using AnimalShelterManagement.Application.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -12,15 +14,18 @@ namespace AnimalShelterManagement.Application.Shelters.Queries.GetShelters
     public class GetSheltersQueryHandler : IRequestHandler<GetSheltersQuery, SheltersVm>
     {
         private readonly IShelterDbContext _context;
+        private readonly IMapper _mapper;
 
-        public GetSheltersQueryHandler(IShelterDbContext shelterDbContext)
+        public GetSheltersQueryHandler(IShelterDbContext shelterDbContext, IMapper mapper)
         {
-                _context = shelterDbContext;
+            _context = shelterDbContext;
+            _mapper = mapper;
         }
 
         public async Task<SheltersVm> Handle(GetSheltersQuery request, CancellationToken cancellationToken)
         {
-            var shelters = await _context.Shelters.AsNoTracking().Where(p => p.StatusId == 1).ToListAsync();
+            var shelters = await _context.Shelters.AsNoTracking().Where(p => p.StatusId == 1).
+                ProjectTo<SheltersVm>(_mapper.ConfigurationProvider).ToListAsync();
 
             return new SheltersVm();
         }
