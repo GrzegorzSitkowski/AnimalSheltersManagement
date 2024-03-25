e8aab3359e24b380b533c3546ec4987ffc0606f1
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-25 19:59:27 +0100

    Added Mapping to ShelterDetailVm

--- AnimalShelterManagement.Application/Shelters/Queries/GetShelterDetail/ShelterDetailVm.cs
+++ AnimalShelterManagement.Application/Shelters/Queries/GetShelterDetail/ShelterDetailVm.cs
@@ -1,4 +1,7 @@
-﻿using AnimalShelterManagement.Domain.ValueObjects;
+﻿using AnimalShelterManagement.Application.Common.Mappings;
+using AnimalShelterManagement.Domain.Entities;
+using AnimalShelterManagement.Domain.ValueObjects;
+using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +10,17 @@ using System.Threading.Tasks;
 
 namespace AnimalShelterManagement.Application.Shelters.Queries.GetShelterDetail
 {
-    public class ShelterDetailVm
+    public class ShelterDetailVm : IMapFrom<Shelter>
     {
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string ShelterAddress { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Shelter, ShelterDetailVm>();
+        }
     }
 }
