44ffe008487ed82ac9076a53b3db8154eb4a01ed
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-26 20:38:28 +0100

    Implemented UpdateShelterCommand

--- AnimalShelterManagement.Application/Shelters/Commands/UpdateShelter/UpdateShelterCommand.cs
+++ AnimalShelterManagement.Application/Shelters/Commands/UpdateShelter/UpdateShelterCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace AnimalShelterManagement.Application.Shelters.Commands.UpdateShelter
 {
-    internal class UpdateShelterCommand
+    public class UpdateShelterCommand : IRequest
     {
+        public Guid ShelterId { get; set; }
     }
 }
