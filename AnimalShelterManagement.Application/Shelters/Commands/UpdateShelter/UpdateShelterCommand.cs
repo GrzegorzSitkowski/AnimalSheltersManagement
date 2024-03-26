86beae4fd933675c902c81c3447b12ed03c84589
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-03-26 20:48:26 +0100

    Implemented UpdateShelterCommandHandler

--- AnimalShelterManagement.Application/Shelters/Commands/UpdateShelter/UpdateShelterCommand.cs
+++ AnimalShelterManagement.Application/Shelters/Commands/UpdateShelter/UpdateShelterCommand.cs
@@ -9,6 +9,13 @@ namespace AnimalShelterManagement.Application.Shelters.Commands.UpdateShelter
 {
     public class UpdateShelterCommand : IRequest
     {
-        public Guid ShelterId { get; set; }
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
